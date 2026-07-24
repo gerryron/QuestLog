801ee7f778f789e5f053499c3a40808d97bd9529
Gerryron <gerryron02@gmail.com>
2026-07-24 18:34:09 +0700

    Add favicon and update PWA manifest to use SVG icon

--- vite.config.ts
+++ vite.config.ts
@@ -25,14 +25,9 @@ export default defineConfig({
         start_url: "/",
         icons: [
           {
-            src: "pwa-192x192.png",
-            sizes: "192x192",
-            type: "image/png",
-          },
-          {
-            src: "pwa-512x512.png",
+            src: "favicon.svg",
             sizes: "512x512",
-            type: "image/png",
+            type: "image/svg+xml",
             purpose: "any maskable",
           },
         ],
